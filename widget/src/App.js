333202112a2ebb9b6fdbed64ebbf890e3494e78a
okbel <curciobel@gmail.com>
2021-02-03 12:12:02 -0300

    Adding widgets

---
+++ widget/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" height="100px" width="100px"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
