69ef506a2008f607667d061ace74d5c8dc0b857e
okbel <curciobel@gmail.com>
2021-02-03 14:06:00 -0300

    Now

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,14 +1,26 @@
 import Head from 'next/head'
 import s from '../styles/Home.module.css'
-import assetManifest from "../../widget/build/asset-manifest"
+import fs from "fs";
+import path from "path";
 
-export default function Home() {
-  console.log(assetManifest)
+export async function getStaticProps() {
+  const assetManifest = await fs.readFileSync(path.join(process.cwd(), "../widget/build/asset-manifest.json"),{encoding:'utf8', flag:'r'})
+
+  return {
+    props: {
+      assetManifest: JSON.parse(assetManifest)
+    }
+  }
+}
+
+export default function Home({ assetManifest }) {
+
   return (
     <div className={s.container}>
       <Head>
         <title>next-microfrontneds</title>
         <link rel="icon" href="/favicon.ico" />
+        <script link="preload" src={`https://next-microfrontend-widget.vercel.app${assetManifest.files["main.js"]}`} />
       </Head>
       <main className={s.main}>
         <div className={s.content}>
